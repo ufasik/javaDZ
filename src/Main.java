501da83cbb2345502c7902904a21a7d668c43e78
ufasik <135876613+ufasik@users.noreply.github.com>
2023-07-15 17:03:15 +0400

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GameResultWriter gameResultWriter = new GameResultWriter();
-        BullsAndCowsGame game = new BullsAndCowsGame(gameResultWriter);
-        game.play();
-    }
-}
